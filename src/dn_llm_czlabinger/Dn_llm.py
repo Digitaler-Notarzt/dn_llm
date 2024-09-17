45dd8e598839745ee58442e06a2b3eca64b692b3
czlabinger <czlabinger@student.tgm.ac.at>
2024-09-17 07:24:07 +0000

    Uses class now

--- src/dn_llm_czlabinger/Dn_llm.py
+++ src/dn_llm_czlabinger/Dn_llm.py
@@ -1,24 +1,22 @@
 from llama_cpp import Llama
 
+class Dn_llm:
 
-def setup(self):
-    self.llm = Llama.from_pretrained(
-        repo_id="akjindal53244/Llama-3.1-Storm-8B-GGUF",
-        filename="Llama-3.1-Storm-8B.Q4_K_M.gguf",
-    )
-
-def question(self):
-    while True:
-        msg = input("Ask a question: ")
-            
-        if(msg == "exit"):
-            quit()
-
-        output = self.llm(
-            "Q: " + msg + " A: ",
-            max_tokens=100,
-            stop=["stop", "\n"],
-            echo=True
+    def __init__(self):
+        self.llm = Llama.from_pretrained(
+            repo_id="akjindal53244/Llama-3.1-Storm-8B-GGUF",
+            filename="Llama-3.1-Storm-8B.Q4_K_M.gguf",
         )
-        print(output)
+
+    def question(self, msg: str): 
+            if(msg == "exit"):
+                quit()
+
+            output = self.llm(
+                "Q: " + msg + " A: ",
+                max_tokens=100,
+                stop=["stop", "\n"],
+                echo=True
+            )
+            print(output)
     
