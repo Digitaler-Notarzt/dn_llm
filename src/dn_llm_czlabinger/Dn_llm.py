474ff519d7e15647a3307c9f635a50f64e05b5a1
czlabinger <czlabinger@student.tgm.ac.at>
2024-09-17 07:14:25 +0000

    Renamed file and moved everything into functions

--- src/dn_llm_czlabinger/Dn_llm.py
+++ src/dn_llm_czlabinger/Dn_llm.py
@@ -1,11 +1,14 @@
 from llama_cpp import Llama
 
-llm = Llama.from_pretrained(
-    repo_id="akjindal53244/Llama-3.1-Storm-8B-GGUF",
-    filename="Llama-3.1-Storm-8B.Q4_K_M.gguf",
-)
+def setup():
+    llm = Llama.from_pretrained(
+        repo_id="akjindal53244/Llama-3.1-Storm-8B-GGUF",
+        filename="Llama-3.1-Storm-8B.Q4_K_M.gguf",
+    )
 
 def question(msg):
+    while True:
+            question(input("Ask a question: "))
     if(msg == "exit"):
         quit()
 
@@ -17,6 +20,4 @@ def question(msg):
     )
     print(output)
 
-if __name__ == "__main__":
-    while True:
-        question(input("Ask a question: "))
+    
