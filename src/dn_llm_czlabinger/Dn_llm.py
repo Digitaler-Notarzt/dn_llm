5171c7caa26a3106fae8ddf18e8ad4cd7fb9df23
czlabinger <czlabinger@student.tgm.ac.at>
2024-09-17 07:20:22 +0000

    Fixed errors

--- src/dn_llm_czlabinger/Dn_llm.py
+++ src/dn_llm_czlabinger/Dn_llm.py
@@ -1,23 +1,24 @@
 from llama_cpp import Llama
 
-def setup():
-    llm = Llama.from_pretrained(
+
+def setup(self):
+    self.llm = Llama.from_pretrained(
         repo_id="akjindal53244/Llama-3.1-Storm-8B-GGUF",
         filename="Llama-3.1-Storm-8B.Q4_K_M.gguf",
     )
 
-def question(msg):
+def question(self):
     while True:
-            question(input("Ask a question: "))
-    if(msg == "exit"):
-        quit()
+        msg = input("Ask a question: ")
+            
+        if(msg == "exit"):
+            quit()
 
-    output = llm(
-        "Q: " + msg + " A: ",
-        max_tokens=100,
-        stop=["stop", "\n"],
-        echo=True
-    )
-    print(output)
-
+        output = self.llm(
+            "Q: " + msg + " A: ",
+            max_tokens=100,
+            stop=["stop", "\n"],
+            echo=True
+        )
+        print(output)
     
