898ec1d697f4b9990b262d40ca56c4f5e5ba34e1
czlabinger <czlabinger@student.tgm.ac.at>
2024-11-05 09:35:13 +0100

    Bumped to 0.6

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.dn_llm_czlabinger.Dn_llm import Dn_llm
 
 def main():
     llm = Dn_llm()
-    llm.question("How are you feeling?", "You are a AI model you dont have feelings.")
+    llm.question(msg="How are you feeling?", system_message="You are a AI model you dont have feelings.")
 
 if __name__ == "__main__":
     main()
